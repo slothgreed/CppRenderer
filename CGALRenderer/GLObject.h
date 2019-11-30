5dc6bb5df934b3c2de2e25082656fa6cc67a7560
slothgreed <stmndrm@gmail.com>
2019-11-30 17:17:28 +0900

    disposeを仮想関数に

--- CGALRenderer/GLObject.h
+++ CGALRenderer/GLObject.h
@@ -4,12 +4,12 @@ class GLObject
 {
 public:
 	GLObject() { m_Id = 0; };
-	~GLObject() { Dispose(); };
+	~GLObject() {};
 
 	virtual void Generate() = 0;
 	virtual void Bind() = 0;
 	virtual void UnBind() = 0;
-	virtual void Dispose() { assert(0); };
+	virtual void Dispose() = 0;
 
 protected:
 	GLuint m_Id;
